7ac6c74f8251e4b1b35c1f8ab84bf14e449673ce
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-12-05 19:23:24 +0100

    the filter method in practice

--- script.js
+++ script.js
@@ -176,3 +176,19 @@ console.log(accounts);
 // accounts.forEach(account => createUserNames(account.owner));
 
 // const user = 'Steven Thomas Williams'; //stw - username
+
+// the FILTER METHOD
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(mov => mov > 0);
+
+console.log(deposits); //zwraca (5) [200, 450, 3000, 70, 1300]
+
+// to samo z for of
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+
+// console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
