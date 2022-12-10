2183ccb5a6098b01ca79609b7679f9db34b9c349
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-12-10 07:43:08 +0100

    my solution of challenge #3

--- script.js
+++ script.js
@@ -322,3 +322,23 @@ createUserNames(accounts);
 //   .reduce((acc, mov) => acc + mov);
 
 // console.log(totaldepositsUSD);
+
+/* Coding Challenge #3
+
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+ */
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(humanAge => humanAge >= 18)
+    .reduce((acc, humAge, i, arr) => acc + humAge / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log('//////////////////////');
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
