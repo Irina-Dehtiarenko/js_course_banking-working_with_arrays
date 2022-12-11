c19500f45b36eac6e36f01a3e47d9c034d41b1e0
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-12-11 19:14:46 +0100

    implemented closing account function with findIndex method

--- script.js
+++ script.js
@@ -438,3 +438,23 @@ btnTransfer.addEventListener('click', e => {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', e => {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = '0';
+    console.log(accounts);
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
