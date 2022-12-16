c8b95e9e58d89f5a860dbceeaebb92d08584aa41
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-12-16 19:03:45 +0100

    array methods practice, part 2

--- script.js
+++ script.js
@@ -699,3 +699,24 @@ const { deposits, withdrawals } = accounts
   );
 
 console.log({ deposits, withdrawals });
+
+// 4.
+// example:
+// this is a nice title -> This Is a Nice Title
+
+const convertTitleText = title => {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+console.log(convertTitleText('this is a nice title'));
+console.log(convertTitleText('this is a LONG title but not too long'));
+console.log(convertTitleText('and here is anoter title with EXAMPLE'));
